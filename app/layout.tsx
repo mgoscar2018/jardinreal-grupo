0ab0540c75e96f1c6c0ca03729bc0678a01c1f2f
Oscar Miranda <mgoscar@gmail.com>
2024-02-27 23:29:06 -0600

    starting project

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+
+import { Nunito } from "next/font/google";
+
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
